7de0fbcade66a2a87f650c9abc4a663b1b35dc99
neerajsinghmahar11 <neeraj.singh.mahar11@gmail.com>
2025-06-06 16:58:55 +0530

    toggle

--- keyboard/todo list using html css/todoList.js
+++ keyboard/todo list using html css/todoList.js
@@ -7,8 +7,28 @@ let todoList=document.querySelector(".todoList");
 const listTodo=()=>{
     todoText=input.value.trim();
     let listItem=document.createElement("li");
+    let label=document.createElement("label");
+    let toggle=document.createElement("input");
+    let span=document.createElement("span");
+    let div1=document.createElement("div");
+    let div2=document.createElement("div");
+    
     if(todoText!=""){
-        listItem.textContent=todoText;
+        listItem.className="list-row"
+        span.textContent=todoText;
+
+        label.className="switchToggle";
+        label.htmlFor="switchToggle";
+
+        toggle.type="checkbox";
+        toggle.name="checkbox";
+        toggle.id="toggle";
+
+        div1.appendChild(span)
+        div2.append(toggle,span)
+        listItem.append(div1,div2)
+        // listItem.appendChild(toggle)
+        // listItem.appendChild(label);
         todoList.appendChild(listItem);
     }
     input.value="";
@@ -17,7 +37,6 @@ const listTodo=()=>{
 
 
 function addList(){
-    console.log(input.value)
      if(input.value!=""){
         addTodoBtn.disabled=false;
     }else{
