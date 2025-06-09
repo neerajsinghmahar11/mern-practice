1df8eb2cf62da74a2c88a9a8802ce9c916ba0860
neerajsinghmahar11 <neeraj.singh.mahar11@gmail.com>
2025-06-09 22:21:20 +0530

    typing game

---
+++ typing-speed-check/typingSpeed.js
@@ -0,0 +1,67 @@
+
+
+const typingText=document.querySelector('.typing-text p');
+const input=document.querySelector('.wrapper .input-field');
+const time=document.querySelector('.time span b');
+const mistakes=document.querySelector('.mistake span');
+const wpm=document.querySelector('.wpm span');
+const cpm=document.querySelector('.cpm span');
+const button=document.querySelector('.button');
+
+
+//set value
+let timer;
+let maxTime=60;
+let timeLeft=maxTime;
+let charIndex=0;
+let mistake=0;
+let isTyping=false;
+
+
+
+
+function loadParagraph(){
+    const paragraph=[
+        "The cat jumped swiftly onto the table, knocking over glasses.",
+        "Rain poured heavily as thunder echoed through the silent night.",
+        "Books lined every wall, creating a maze of paper stories.",
+        "Children played soccer, shouting joyfully under the setting summer sun.",
+        "She whispered secrets into the wind, hoping for answers.",
+        "The ship sailed quietly, moonlight dancing on the calm water.",
+        "Birds chirped loudly while the dog barked at passing squirrels.",
+        "Time slipped away unnoticed, leaving behind echoes of forgotten dreams.",
+        "He searched everywhere, but the keys remained lost forever.",
+    ]
+
+    const randomIndex=Math.floor(Math.random()*paragraph.length)
+    typingText.innerHTML="";
+
+    for(const char of paragraph[randomIndex]){
+        console.log(char);
+        typingText.innerHTML+=`<span>${char}<span/>`;
+    }
+
+    typingText.querySelectorAll('span')[0].classList.add('active');
+}
+
+//handle user input
+function  initTyping(){
+    const char=typingText.querySelectorAll('span');
+    const typedChar=input.value.charAt(charIndex);
+
+    if(charIndex < char.length && timeLeft > 0){
+        if(char[charIndex].innerText === typedChar){
+            char[charIndex].classList.add('correct');
+            console.log('correct')
+        }else{
+            char[charIndex].classList.add('incorrect');
+            console.log('incorrect')
+        }
+        charIndex++;
+    }else{
+
+    }
+}
+
+input.addEventListener("input",initTyping);
+loadParagraph()
